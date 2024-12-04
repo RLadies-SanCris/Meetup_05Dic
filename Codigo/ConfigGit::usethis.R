96793153f48915bb2a110f77b6f181856aeefb36
acanedo92 <anahi.canedotx@gmail.com>
2024-12-04 11:10:02 -0600

    add directorio codigo y script

---
+++ Codigo/ConfigGit::usethis.R
@@ -0,0 +1,28 @@
+# Este es un script de R ? 
+
+install.packages("usethis")​
+
+library(usethis)​
+
+use_git_config(user.name = "Jane Doe", user.email = "jane@example.org")​
+
+​
+
+usethis::create_github_token() ​
+
+​
+
+gitcreds::gitcreds_set() ​
+
+​
+
+usethis::git_default_branch_configure() ​
+
+​
+
+usethis::git_default_branch_configure() ​
+
+​
+
+git config --global init.defaultBranch main​
+
