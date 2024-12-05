da53bf335e5a0fdc516255b9a2042f75711556e8
RLadies-SanCris <rladies.sancris.mx@gmail.com>
2024-12-05 13:29:53 -0600

    reload files

--- Codigo/ConfigGit::usethis.R
+++ Codigo/ConfigGit::usethis.R
@@ -1,28 +1,9 @@
-# Este es un script de R ? 
+# Configurar cuenta de Git con R:
+install.packages("usethis")
+library(usethis)
+use_git_config(user.name = "RLadies-SanCris", user.email = "rladies.sancris.mx@gmail.com")
+usethis::create_github_token() 
+gitcreds::gitcreds_set() 
 
-install.packages("usethis")​
 
-library(usethis)​
-
-use_git_config(user.name = "Jane Doe", user.email = "jane@example.org")​
-
-​
-
-usethis::create_github_token() ​
-
-​
-
-gitcreds::gitcreds_set() ​
-
-​
-
-usethis::git_default_branch_configure() ​
-
-​
-
-usethis::git_default_branch_configure() ​
-
-​
-
-git config --global init.defaultBranch main​
 
